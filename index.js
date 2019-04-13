79d673e9cfc8646f66f95e4cab4ec5a6f1fd097c
Ricardo Ortega <rikrdo.ortega@gmail.com>
2019-04-13 23:18:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ function destructivelyRemoveFirstKitten(){
 }
 
 function appendKitten(name){
+  var newArray = [...kittens, name]
+  return newArray
+}
+
+function prependKitten(name){
   var newArray = [name, ...kittens]
   return newArray
 }
