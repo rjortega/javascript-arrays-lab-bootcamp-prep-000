939ad21a4e00d3e95915207b8f2e7455168bdf83
Ricardo Ortega <rikrdo.ortega@gmail.com>
2019-04-13 23:22:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,3 +31,8 @@ function removeLastKitten(){
   var newArray = kittens.slice(0, kittens.length -1)
   return newArray
 }
+
+function removeFirstKitten(){
+  var newArray = kittens.slice(1)
+  return newArray
+}
